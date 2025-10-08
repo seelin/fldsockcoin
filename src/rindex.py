e4028adfecd1cdbc088fee1fd357959a43a3b305
seelin <seelin@126.com>
2025-10-08 21:48:06 +0800

    Delete src directory

--- src/rindex.py
+++
@@ -1,9 +0,0 @@
-
-from fastapi import FastAPI, Request
-from workers import WorkerEntrypoint
-
-
-class Default(WorkerEntrypoint):    
-     async def fetch(self, request):        
-          name = (await request.json()).name        
-          return Response(f"name:{name}")
