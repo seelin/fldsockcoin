cdb4108cd29edb710507479392592cd7e3112b7a
seelin <seelin@126.com>
2025-10-08 14:36:51 +0800

    Update rindex.py

--- src/rindex.py
+++ src/rindex.py
@@ -1,1 +1,9 @@
 
+from fastapi import FastAPI, Request
+from workers import WorkerEntrypoint
+
+
+class Default(WorkerEntrypoint):    
+     async def fetch(self, request):        
+          name = (await request.json()).name        
+          return Response(f"name:{name}")
