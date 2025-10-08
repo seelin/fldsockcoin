49ac671c5d0b1f83bb7508460eaceb554bcb51f3
seelin <seelin@126.com>
2025-10-08 23:49:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ export default {
     SK_ESTOKEN = env.SK_ESTOKEN||ekv.get('SK_ESTOKEN');
     SK_CMCKEY = env.SK_CMCKEY||ekv.get('SK_CMCKEY');
     SK_EXCKEY = ekv.SK_EXCKEY||ekv.get('SK_EXCKEY');
-    //SK_EXCKEY=JSON.stringify(ekv);
+    
     return await handleErrors(request, async () => {
       let url = new URL(request.url);
       let path = url.pathname.slice(1).split('/');
