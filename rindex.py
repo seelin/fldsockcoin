7f59c403d238e2e6dcae77cef1146ce4bc6f10a1
seelin <seelin@126.com>
2025-10-08 13:16:13 +0800

    Update rindex.py

--- rindex.py
+++ rindex.py
@@ -1,3 +1,6 @@
 import jinja2
 from fastapi import FastAPI, Request
 from workers import WorkerEntrypoint
+
+environment = jinja2.Environment()
+template = environment.from_string("Hello, {{ name }}!")
